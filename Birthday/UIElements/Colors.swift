f6609500f0d64f7effa42cf88111a9912ac09d87
Sirarpi Bayramyan <sirarpibayramyan@Sirarpis-MacBook-Pro.local>
2023-08-21 14:55:05 +0400

    ,

--- Birthday/UIElements/Colors.swift
+++ Birthday/UIElements/Colors.swift
@@ -1,11 +1,10 @@
-
 import SwiftUI
 
 extension Color {
-  
+
   static let primaryColor = Color("PrimaryColor")
   static let secondaryColor = Color("SecondaryColor")
   static let tertiaryColor = Color("TertiaryColor")
   static let backgroundColor = Color("BackgroundColor")
-  
+
 }
--- Birthday/UIElements/Logo.swift
+++ Birthday/UIElements/Logo.swift
@@ -1,14 +1,13 @@
-
 import SwiftUI
 
 struct Logo: View {
-  
+
   var imageName = "logo"
-  
+
   var body: some View {
     Image("LogoBirthApp")
       .resizable()
       .aspectRatio(contentMode: .fit)
   }
-  
+
 }
