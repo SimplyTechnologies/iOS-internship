a1b16e0dff7b594d2a94dc9be9a927fe4333d610
Sirarpi Bayramyan <sirarpibayramyan@Sirarpis-MacBook-Pro.local>
2023-08-22 16:56:25 +0400

    Integrated Swift lint

--- Birthday/Services/RequestManager/RequestServices.swift
+++ Birthday/Services/RequestManager/RequestServices.swift
@@ -3,7 +3,7 @@ import Apollo
 import Foundation
 
 enum RequestServices {
-  
+  // swiftlint:disable:next type_name
   struct API {
     static let baseUrl: URL = URL(string: "https://simply-mobile.amiryan.online/graphql")!
   }
