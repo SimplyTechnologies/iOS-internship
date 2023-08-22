28824fd948a9342379683d6e53bf22df6fb737f0
Edgar <arakelyanedgar98@gmail.com>
2023-08-22 16:49:00 +0400

    Tab Bar Implementation

---
+++ Birthday/Modules/TabBar/TabViewTab.swift
@@ -0,0 +1,25 @@
+
+import SwiftUI
+
+struct TabViewTab: View {
+  
+  @Binding var activeTab: TabItem
+  var tab: TabItem
+  
+  var body: some View {
+    activeTab == tab ? tab.selectedImage : tab.unSelectedImage
+  }
+  
+}
+
+// MARK: - Preview
+
+struct TabViewTab_Previews: PreviewProvider {
+  
+  @State static var activeTab: TabItem = .home
+  
+  static var previews: some View {
+    TabViewTab(activeTab: $activeTab, tab: .home)
+  }
+  
+}
