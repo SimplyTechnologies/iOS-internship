846ce4f06bbcc949ecc330ab69418adf2c172de7
ShushanikBarseghyan <36878578+ShushanikBarseghyan@users.noreply.github.com>
2023-09-07 18:44:38 +0400

    Navigation and kerning (#31)

---
+++ Birthday/UIElements/Kerning/KerningView.swift
@@ -0,0 +1,47 @@
+
+import SwiftUI
+
+struct KerningTextField: UIViewRepresentable {
+  
+  @Binding var text: String
+  var placeholder: String
+  var kerning: CGFloat
+  
+  func makeUIView(context: Context) -> UITextField {
+    let textField = UITextField()
+    textField.delegate = context.coordinator
+    textField.textAlignment = .center
+    textField.font = UIFont.init(name: Font.Karma.bold.rawValue, size: 18)
+    textField.placeholder = placeholder
+    textField.textColor = UIColor(Color.primaryColor)
+    
+    return textField
+  }
+  
+  func updateUIView(_ uiView: UITextField, context: Context) {
+    uiView.text = text
+    let attributedText = NSAttributedString(
+      string: text, attributes: [NSAttributedString.Key.kern: kerning]
+    )
+    uiView.attributedText = attributedText
+  }
+  
+  func makeCoordinator() -> Coordinator {
+    Coordinator(self)
+  }
+  
+  class Coordinator: NSObject, UITextFieldDelegate {
+    
+    var parent: KerningTextField
+    
+    init(_ parent: KerningTextField) {
+      self.parent = parent
+    }
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+      parent.text = textField.text ?? ""
+    }
+    
+  }
+  
+}
